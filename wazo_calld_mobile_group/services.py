165a8b71aa4311d49c990bf7f8eb3966d849eeaa
Sylvain Boily <sylvainboilydroid@gmail.com>
2022-03-02 22:31:33 -0500

    patching dao is no longer needed

--- wazo_calld_mobile_group/services.py
+++ wazo_calld_mobile_group/services.py
@@ -12,13 +12,32 @@ class WP465Service():
         self.amid = amid
 
     def enable_devstate(self, uuid):
+        endpoint = self._get_endpoint(uuid)
+        self.amid.action('DialplanExtensionAdd', {
+            'Context': 'usersharedlines',
+            'Priority': 'hint',
+            'Extension': uuid,
+            'Application': f"Custom:{uuid}&{endpoint}",
+            'Replace': 'yes'
+        })
+
         self.amid.action('setVar', {
             'Variable': f"DEVICE_STATE(Custom:{uuid})",
             'Value': 'NOT_INUSE'
         })
 
     def disable_devstate(self, uuid):
+        endpoint = self._get_endpoint(uuid)
         self.amid.action('setVar', {
             'Variable': f"DEVICE_STATE(Custom:{uuid})",
             'Value': 'UNAVAILABLE'
         })
+
+    def _get_endpoint(self, uuid):
+        endpoint = self.amid.action('ExtensionState', {
+            'Context': 'usersharedlines',
+            'Exten': uuid,
+        })
+
+        interfaces = endpoint[0]['Hint'].split('pjsip')
+        return "&".join([match for match in interfaces if "pjsip" in match])
